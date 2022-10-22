cf88804db0042d8d762d6e1bd7e9961fe5ef86a7
Armaan101010 <116051448+Armaan101010@users.noreply.github.com>
2022-10-22 11:25:52 +0530

    Add files via upload

---
+++ overlay.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("filters-overlay").style.display = "block";
+  }
+  
+function closeNav() {
+    document.getElementById("filters-overlay").style.display = "none";
+}
+
+  
